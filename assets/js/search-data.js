ef8bb95a2b52aaba8211aab5147f2285cbd58c90
DonggeonDavidOh <129044411+DonggeonDavidOh@users.noreply.github.com>
2025-11-04 21:01:47 +0000

    Deploying to gh-pages from @ DonggeonDavidOh/DonggeonDavidOh.github.io@31f32ed1f233c269f0572db073b3e2dfa209e2e4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please click the PDF icon to open or download my most up-to-date CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
